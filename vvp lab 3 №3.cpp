d1b400ce9b8299cb0a2fabc9bbb284b77b992e50
Валерия <valeria103020@gmail.com>
2020-10-01 11:53:52 +0300

    Добавьте файлы проекта.

---
+++ vvp lab 3 №3.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	float a, b;
+	float S;
+	printf("Enter a,please: ");
+	scanf_s("%f", &a);
+	printf("Enter b,please: ");
+	scanf_s("%f", &b);
+	S = (a + b) / 2;
+	printf("%f", S);
+	return 0;
+}
